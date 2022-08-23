04c69695d637af0f8f0ec9c13b85df21222c5647
actuallyakshay <akshay.rajput1197@gmail.com>
2022-08-24 02:24:35 +0530

    added files

---
+++ components/navbar.js
@@ -0,0 +1,55 @@
+function navbar(){
+    return ` <div class="flex">
+    <div class="logo"><img
+            src="https://assets-global.website-files.com/6257f2528a39952d4e8af286/6257f2528a39955aef8af720_timely-new-logo.svg"
+            alt=""></div>
+    <ul>
+        <li><a href="/">Features ⌵</a>
+            <ul >
+                <li><a href=""> Track Time</a></li>
+                <li><a href=""> Track Projects</a></li>
+                <li><a href=""> Track Teams</a></li>
+                <li><a href=""> Track Plans</a></li>
+                <li><a href=""> Integrations</a></li>
+                <li><a href="">Our privacy promise</a></li>
+            </ul>
+        </li>
+        <li><a href="">Solutions ⌵</a>
+            <ul>
+                <li><a href="">Consulting Firms</a></li>
+                <li><a href="">Software Companies</a></li>
+                <li><a href="">Agencies</a></li>
+                <li><a href="">Designers & Creatives</a></li>
+                <li><a href="">Lawyers</a></li>
+                <li><a href="">Accountants</a></li>
+                <li><a href="">Teams</a></li>
+                <li><a href="">Freelancers</a></li>
+                <li><a href="">Employees</a></li>
+                <li><a href="">Project Managers</a></li>
+                <li><a href="">Remote workers</a></li>
+            </ul>
+        </li>
+        <li><a href="">Pricing</a> </li>
+        <li><a href="">Resources ⌵</a>
+            <ul>
+                <li><a href="">Blog</a></li>
+                <li><a href="">Case studies</a></li>
+                <li><a href="">Podcast</a></li>
+                <li><a href="">Books</a></li>
+                <li><a href="">Webinars</a></li>
+                <li><a href="">Time traking guide</a></li>
+                <li><a href="">Support</a></li>
+            </ul>
+        </li>
+        <li><a href="">Talk to sales</a></li>
+        <li><a href="/">Login</a></li>
+        <li><a href="" id="free_trial">Start 14-day free trial</a></li>
+        <li><a href=""> <img id="about"
+                    src="https://assets-global.website-files.com/6257f2528a39952d4e8af286/6257f2528a399514908af72d_made-by-white.svg"
+                    alt=""></a> </li>
+
+    </ul>
+</div>`
+};
+
+export default navbar;
