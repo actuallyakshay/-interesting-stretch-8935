8f6ab27b970eda1a4de07dfa0383d5a77cd2a32c
actuallyakshay <akshay.rajput1197@gmail.com>
2022-08-24 19:27:36 +0530

    Added files

--- components/signup&loginNavbar.js
+++ components/signup&loginNavbar.js
@@ -1,13 +1,13 @@
 function navbar(){
-    return `<div id="logo">
-    <img src="https://d1luwo5u9zpc4i.cloudfront.net/assets/memory_logo-f1db7b16e667aab7b9a607429d44d5cc17125da4f36faac751017cdb71ea8ac2.svg"
-        alt="">
+    return `<div id="logo"><a href="./index.html"><img src="https://d1luwo5u9zpc4i.cloudfront.net/assets/memory_logo-f1db7b16e667aab7b9a607429d44d5cc17125da4f36faac751017cdb71ea8ac2.svg"
+    alt=""></a>
+    
 </div>
-<div id="logo1">
-    <img src="https://d1luwo5u9zpc4i.cloudfront.net/assets/memory_apps/timely_icon-0009454e9497f1cb2a0f9042037156a62c31421032858278d5002aae979084a2.svg"
-        alt="">
+<div id="logo1"><a href="./index.html"><img src="https://d1luwo5u9zpc4i.cloudfront.net/assets/memory_apps/timely_icon-0009454e9497f1cb2a0f9042037156a62c31421032858278d5002aae979084a2.svg"
+alt=""></a>
+    
 </div>
 <div></div>`
 }
 
-export default  navbar
+export default  navbar;
